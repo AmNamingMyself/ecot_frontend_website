30854a380a6a649d467b50c5ec0b74226888a649
MrNyathi <nyathiexpounds@gmail.com>
2026-02-13 15:35:34 +0200

    there was some car services here idont know how

---
+++ tore header.php
@@ -0,0 +1,55 @@
+[33mb907d6f[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m Merge branch 'main' of https://github.com/AmNamingMyself/ecot_frontend_website
+[33md9e0d2d[m menu bar
+[33m08cd1c6[m redundency in responsive css
+[33mc7704ae[m trying responsiveness
+[33m20a677d[m Merge branch 'main' of https://github.com/AmNamingMyself/ecot_frontend_website
+[33me48bbb3[m added the certificates and partime programs..
+[33mb397830[m Merge branch 'main' of https://github.com/AmNamingMyself/ecot_frontend_website
+[33mbb329bf[m updated foooter, changed logo in footer, and script for homslider and alerts
+[33m64ed5a2[m changed navigation and admission page. modified links
+[33m0fc3d0f[m added /applicationto all ApplyNow
+[33md14a8ef[m Added /application
+[33m9fc26f4[m governance to principal officers
+[33mfb9ecbc[m `Updated governance.php and included menu-nav.php to display college management
+[33me232f85[m Merge branch 'main' of https://github.com/AmNamingMyself/ecot_frontend_website
+[33m105ea15[m updates SRC, apply and tution pages
+[33m3d05532[m modified about.
+[33mc654de7[m Merge branch 'main' of https://github.com/AmNamingMyself/ecot_frontend_website
+[33m1edee87[m Admin setup...
+[33m70b7ccf[m Merge branch 'main' of https://github.com/AmNamingMyself/ecot_frontend_website
+[33mb657091[m added info in history
+[33m830fcc9[m academic
+[33m36b7ca0[m Merge branch 'main' of https://github.com/AmNamingMyself/ecot_frontend_website
+[33mc775a76[m Merge branch 'main' of https://github.com/AmNamingMyself/ecot_frontend_website
+[33m375a48e[m index, changed dates, and one picture
+[33m15e7936[m hidden some tabs whi are empty
+[33mcf02c99[m added SRC page
+[33m6f781e2[m just
+[33m0d02f80[m not sure what i did
+[33m329f636[m Merge branch 'main' of https://github.com/AmNamingMyself/ecot_frontend_website
+[33m6a45c51[m updated dates in index
+[33mcd04266[m Merge branch 'main' of https://github.com/AmNamingMyself/ecot_frontend_website
+[33m59acd43[m editted the engineering science page
+[33md8408ca[m look the research
+[33m1602d3e[m hidded some info info in menu-nav. added links to pages wihout links
+[33mc838e24[m resized slider image
+[33m9329905[m picture integration
+[33mf613bd8[m piictures
+[33m33ce66e[m aligned the links above the footer
+[33m9f37396[m made links on and changed some contents
+[33m5e9566f[m look loAcademics but khona lokushodako
+[33m7c202a0[m added tuition.php, programs.php, apply, and admissions. Still have to get all data from database.
+[33m2174ddf[m made the admissions and tuition pages. i'd like some assistance with styling
+[33m3184a8b[m added the conatct page. missing functionality
+[33m1b2f59c[m added ict, engineering and building pages under academic.
+[33m0f8a9bd[m added business and education pages.
+[33m311f182[m Merge branch 'main' of https://github.com/AmNamingMyself/ecot_frontend_website
+[33mae3d992[m and images
+[33md95febf[m Merge branch 'main' of https://github.com/AmNamingMyself/ecot_frontend_website
+[33m73e6abf[m added about pages and hid other links. solved map issue
+[33m3c8c6ae[m lateest modifications
+[33m9b2d1dc[m watch the space
+[33mcd284a8[m please look the page not the colors
+[33mb2c47ab[m updates
+[33m8f3cbc6[m created an updated folder where our work is
+[33mf7b017d[m Initial commit
